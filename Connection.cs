8256f1a243919017c53cd3b28b7ff1440ae2f158
Afroza Yasmin Mily <mily.ku10@gmail.com>
2015-09-09 02:10:09 +0600

    Create Connection.cs

---
+++ Connection.cs
@@ -0,0 +1,15 @@
+# LG-Storage-Management-System
+LG Showroom Employee Database Management System using Visual Studio 2010
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LG_Storage
+{
+    class Connection
+    {
+        String connectionString="Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\pc\\Documents\\Log_In.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+    }
+}
