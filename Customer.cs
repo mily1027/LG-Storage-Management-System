937498dee92e6e2e828dff9945098680e2e1cb91
Afroza Yasmin <mily.ku10@gmail.com>
2015-09-08 13:12:12 +0600

    Create Customer.cs

---
+++ Customer.cs
@@ -0,0 +1,32 @@
+# LG-Storage-Management-System
+LG Showroom Employee Database Management System using Visual Studio 2010
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LG_StorageFinal
+{
+    public partial class Customer : Form
+    {
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
