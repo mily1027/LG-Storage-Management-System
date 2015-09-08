a9a3a90a355b86b07ee5771e76df10e56f3adc41
Afroza Yasmin <mily.ku10@gmail.com>
2015-09-08 13:11:42 +0600

    Create Accounts.cs

---
+++ Accounts.cs
@@ -0,0 +1,22 @@
+# LG-Storage-Management-System
+LG Showroom Employee Database Management System using Visual Studio 2010
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LG_StorageFinal
+{
+    public partial class Accounts : Form
+    {
+        public Accounts()
+        {
+            InitializeComponent();
+        }
+    }
+}
